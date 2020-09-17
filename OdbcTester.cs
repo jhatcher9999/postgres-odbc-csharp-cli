1f50e971305634b2314cd6ed81367725c974f01c
Jim Hatcher <hatcher@cockroachlabs.com>
2020-09-17 15:38:36 -0500

    lots of changes to tests

--- OdbcTester.cs
+++ OdbcTester.cs
@@ -1,35 +1,88 @@
 using System;
 using System.Data.Odbc;
 using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace postgres_odbc_csharp_cli
 {
     class OdbcTester
     {
 
-        private string _connString = "DSN=PostgreSQL;Trusted_Connection=Yes";
-        //private string _connString = "Driver={PostgreSQL};Server=localhost;Port=26527;Database=test;Uid=;Pwd=;";
-        //private string _connString = "DSN=PostgreSQL;UID='';PWD=''";
+        private string _connString = "DSN=CockroachDB;Trusted_Connection=Yes";
 
         private Random random = new Random((int)DateTime.Now.Ticks);
 
 
         public void Run()
         {
-            Console.WriteLine("Starting TEST 1: Querying Data");
+
+            int testNumber = 1;
+
+            Console.WriteLine("Starting TEST " + testNumber++ + ": Connection Options");
             Console.WriteLine("=============================================");
             Console.WriteLine();
-            QueryData();
+            ConnectionOptions();
+
+            Console.WriteLine("Starting TEST " + testNumber++ + ": Querying Data");
+            Console.WriteLine("=============================================");
+            Console.WriteLine();
+            ReadData();
 
             int maxId = GetMaxCustomerID();
 
-            Console.WriteLine("Starting TEST 2: Inserting Data");
+            Console.WriteLine("Starting TEST " + testNumber++ + ": Inserting Data");
             Console.WriteLine("=============================================");
             Console.WriteLine();
-            InsertData(maxId + 1);
+            WriteData(maxId + 1);
         }
 
-        private void QueryData()
+        private void ConnectionOptions()
+        {
+
+            Dictionary<string, string> connStrings = new Dictionary<string, string> {
+                { "DSN=CockroachDB",                                                                              "DSN with no authentication options given" },
+                { "DSN=CockroachDB;Trusted_Connection=Yes",                                                       "DSN with trusted connection" },
+                { "DSN=CockroachDB;Uid=;Pwd=",                                                                    "DSN with blank username/password" },
+                { "DSN=CockroachDB;Uid=jimhatcher;Pwd=",                                                          "DSN with username specified but a blank password" },
+                { "Driver={PostgreSQL Driver};Server=localhost;Port=26257;Database=test;Trusted_Connection=Yes;", "DSN-less with trusted connection" },
+                { "Driver={PostgreSQL Driver};Server=localhost;Port=26257;Database=test;Uid=;Pwd=;",              "DSN-less with blank username/password" },
+            };
+
+            foreach(KeyValuePair<string, string> kvp in connStrings)
+            {
+                string connStringValue = kvp.Key;
+                string connStringDescription = kvp.Value;
+
+                Console.WriteLine("Attempting connection ...");
+                Console.WriteLine("Connection String: " + connStringValue);
+                Console.WriteLine("Scenario: " + connStringDescription);
+                OdbcConnection connection = new OdbcConnection(connStringValue);
+                try
+                {
+
+                    connection.Open();
+
+                    Console.WriteLine("Success");
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Failure");
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    // Console.WriteLine("Connection State: " + connection.State);
+                    // Console.WriteLine();
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+
+        private void ReadData()
         {
 
             OdbcConnection connection = new OdbcConnection(_connString);
@@ -43,7 +96,7 @@ namespace postgres_odbc_csharp_cli
                 OdbcDataReader reader = null;
                 try
                 {
-                    string query = "SELECT * FROM test.customer ORDER BY customer_id DESC LIMIT 10;";
+                    string query = "SELECT * FROM test.customer ORDER BY customer_id DESC LIMIT 5;";
                     OdbcCommand command = new OdbcCommand(query, connection);
 
                     reader = command.ExecuteReader();
@@ -60,6 +113,10 @@ namespace postgres_odbc_csharp_cli
                         }
                         Console.WriteLine();
                     }
+
+                    Console.WriteLine("Read successful");
+                    Console.WriteLine();
+                    
                 }
                 catch(Exception ex)
                 {
@@ -87,10 +144,10 @@ namespace postgres_odbc_csharp_cli
             }
         }
 
-        private void InsertData(int customerID)
+        private void WriteData(int customerID)
         {
             string name = GetRandomString(5);
-            InsertData(customerID, name);
+            WriteData(customerID, name);
         }
 
         private string GetRandomString(int length)
@@ -107,7 +164,7 @@ namespace postgres_odbc_csharp_cli
             return builder.ToString();
         }
 
-        private void InsertData(int customerID, string name)
+        private void WriteData(int customerID, string name)
         {
 
             OdbcConnection connection = new OdbcConnection(_connString);
@@ -127,6 +184,36 @@ namespace postgres_odbc_csharp_cli
                     command.Parameters.Add("@name", OdbcType.VarChar).Value = name;
 
                     Console.WriteLine("Running INSERT query with values " + customerID.ToString() + ", " + name);
+                    command.ExecuteNonQuery();
+
+                    Console.WriteLine("INSERT successful");
+                    Console.WriteLine();
+
+                    string query2 = "UPDATE test.customer SET name = ? WHERE customer_id = ?;";
+                    string newName = GetRandomString(5);
+                    OdbcCommand command2 = new OdbcCommand(query2, connection);
+
+                    command2.Parameters.Add("@name", OdbcType.VarChar).Value = newName;
+                    command2.Parameters.Add("@customer_id", OdbcType.Int).Value = customerID;
+
+                    Console.WriteLine("Running UPDATE query with values " + customerID.ToString() + ", " + newName);
+                    command2.ExecuteNonQuery();
+
+                    Console.WriteLine("UPDATE successful");
+                    Console.WriteLine();
+
+                    string query3 = "DELETE FROM test.customer WHERE customer_id = ?;";
+                    OdbcCommand command3 = new OdbcCommand(query3, connection);
+
+                    command3.Parameters.Add("@customer_id", OdbcType.Int).Value = customerID;
+
+                    Console.WriteLine("Running DELETE query with values " + customerID.ToString());
+                    command3.ExecuteNonQuery();
+
+                    Console.WriteLine("DELETE successful");
+                    Console.WriteLine();
+
+                    //put the data back so we will keep some values around for our read queries
                     command.ExecuteNonQuery();
 
                 }
